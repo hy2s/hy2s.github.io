0a06ea9a0f0b1751601e1bfb0d29e277528c7ac0
김혜승 <rlagptmd1021@naver.com>
2025-05-21 17:34:15 +0900

    pinterest
    create pinterest

---
+++ pinterest/script/data.js
@@ -0,0 +1,17 @@
+const imgList = [
+  { type:'good', img:'images/p1.png', title:'Title-1', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p2.png', title:'Title-2', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p3.png', title:'Title-3', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p4.png', title:'Title-4', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p5.png', title:'Title-5', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p6.png', title:'Title-6', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p7.png', title:'Title-7', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p8.png', title:'Title-8', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p9.png', title:'Title-9', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p10.png', title:'Title-10', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p11.png', title:'Title-11', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p12.png', title:'Title-12', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p13.png', title:'Title-13', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'bad', img:'images/p14.png', title:'Title-14', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+  { type:'good', img:'images/p15.png', title:'Title-15', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, rerum.' },
+];
